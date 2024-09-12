10dff1c97254eb6d902b00b58a33b713724a88dc
JohnPaolo18 <Paolokahlil@gmail.com>
2024-09-11 21:00:15 -0600

    Added Header and week 2 link
    Added heading cprg306: web dev 2 and link to the week 2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Paolo Bermudez</h1>
+      <p>
+      <Link href ="https://github.com/JohnPaolo18">My Github Repository</Link>
+      </p>              
+    </main>
+  );
+}
